b65d01a7caea5e64d48eaba0de57ba57045e8152
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2023-01-09 16:41:49 +0000

    Update server.js

--- XXE & Deserialisation/server.js
+++ XXE & Deserialisation/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const fileUpload = require('express-fileupload');
-const libxmljs = require("libxmljs");
-const serialize = require('node-serialize')
-const ejs = require('ejs')
+const fileUpload = require('express-fileupload');   /* access uploaded files in the `req.files` object */
+const libxmljs = require("libxmljs");               /* xml parser */
+const serialize = require('node-serialize')         /* (de)serialize javascript objects */
+const ejs = require('ejs')                          /* templating engine for dynamic content in the views */
 
 const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public'));                  /* middleware for serving static (client-side) files from public folder 
 app.use(fileUpload())
-app.use(express.json())
+app.use(express.json())                             /* parse json in request body */
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -16,20 +16,25 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+    /* check if .xml file uploaded */
+    
     if (req.files && req.files.products && req.files.products.mimetype == 'text/xml') {
         var xmlFile = req.files.products.data.toString('utf-8')
         var parsedXml = libxmljs.parseXmlString(xmlFile, { noent:true, noblanks:true })
         var products = parsedXml.root().childNodes().map(product => product.childNodes()[0].text())
         return res.render('index', { products: products })
     }
+    /* otherwise check for json body and parse the item out */
+    
     else if (req.body.item) {
         var product = serialize.unserialize(req.body).item
 
         try {
-          var product = product.split()
-          return res.render('index', { products: product })
+            var product = product.split()
+            return res.render('index', { products: product })
 
-        } catch (error) {
+        } 
+        catch (error) {
             console.log(error)
             return res.status(500)
         }
