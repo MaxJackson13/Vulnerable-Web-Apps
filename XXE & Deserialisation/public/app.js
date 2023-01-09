a220eb131acd42693fa400fe56ca5de5093e03d6
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2023-01-09 16:35:54 +0000

    Update app.js

--- XXE & Deserialisation/public/app.js
+++ XXE & Deserialisation/public/app.js
@@ -1,12 +1,20 @@
+/*select form & html elements as well assubmit button*/
+
 var originalDoc = document.querySelector('html')
 const form = document.getElementById('form')
 const button = document.querySelector('.search-button');
 
+/* listen for click event on the submit button */
+
 button.addEventListener('click', event => {
   event.preventDefault()
 
+  /* extract submitted formdata*/
+  
   const formData = new FormData(form)
   const product = formData.get('item')
+  
+  /*construct json post request */
   
   fetch('http://localhost:5000/', {
     method: 'POST',
@@ -16,6 +24,9 @@ button.addEventListener('click', event => {
     body: JSON.stringify({item: `${product}`})
   })
   .then((response) => response.text())
+  
+  /*parse the  response and replace the webpage html with the fecthed html */
+  
   .then((html) => {
     var parser = new DOMParser()
     var newDoc = parser.parseFromString(html, 'text/html')
