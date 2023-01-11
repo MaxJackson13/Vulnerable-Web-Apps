ca1e0fae91852f22d549d978a66abcbda8807f3e
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2023-01-11 21:45:59 +0000

    Create middleware.js

---
+++ SSRF/middleware.js
@@ -0,0 +1,50 @@
+const ipAddr = require('ipaddr.js')
+const axios = require('axios')
+
+async function axiosGet (req, url) {
+    try {
+        const response = await axios.get(url)
+        req.health = { 
+            statusCode: response.status,
+            body: response.data
+        }
+    } catch (err) {
+        req.health = {
+            statusCode: err.message || err.code,
+            body: ""
+        }
+    }
+}
+
+async function fetchUrl(req, res, next) {
+    try {
+        const webhookURL = req.body.webhookURL
+        const ip = new URL(webhookURL).hostname
+        if (ipAddr.isValid(ip)) {
+            const range = ipAddr.parse(ip).range()
+            if (range === 'loopback') {
+                req.health = {
+                    statusCode: 'ip not allowed',
+                    body: ""
+                }
+            } else {
+                await axiosGet(req, webhookURL)
+            } 
+
+        } else if (ip.toLowerCase() === 'localhost') {
+            req.health = {
+                statusCode: 'ip not allowed',
+                body: ""
+            }
+        } else {
+            await axiosGet(req, webhookURL)
+        }
+    }
+    catch (err) {
+        req.health = {
+            statusCode: err.message || err.code,
+            body: ""
+        }
+    }  
+    next() 
+}
