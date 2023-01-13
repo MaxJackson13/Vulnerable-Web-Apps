37682e27e0605ff05a7a7b46a7d5a3f111354410
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2023-01-13 16:03:40 +0000

    Update server.js

--- XXE & Deserialisation/server.js
+++ XXE & Deserialisation/server.js
@@ -1,41 +1,44 @@
 const express = require('express')
-const fileUpload = require('express-fileupload');   /* access uploaded files in the `req.files` object */
-const libxmljs = require("libxmljs");               /* xml parser */
-const serialize = require('node-serialize')         /* (de)serialize javascript objects */
-const ejs = require('ejs')                          /* templating engine for dynamic content in the views */
+const fileUpload = require('express-fileupload');
+const libxmljs = require("libxmljs");
+const serialize = require('node-serialize')
+const ejs = require('ejs')
 
 const app = express()
 
-app.use(express.static('public'));                  /* middleware for serving static (client-side) files from public folder */ 
+app.use(express.static('public'));
 app.use(fileUpload())
-app.use(express.json())                             /* parse json in request body */
+app.use(express.json())
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index', { products: false })
 })
 
-app.post('/', (req, res) => {                                                                       
-    if (req.files && req.files.products && req.files.products.mimetype == 'text/xml') {                 /* check if .xml file uploaded */
+app.post('/', (req, res) => {
+    if (req.files && req.files.products && req.files.products.mimetype == 'text/xml') {
         var xmlFile = req.files.products.data.toString('utf-8')
         var parsedXml = libxmljs.parseXmlString(xmlFile, { noent:true, noblanks:true })
         var products = parsedXml.root().childNodes().map(product => product.childNodes()[0].text())
         return res.render('index', { products: products })
     }
-    else if (req.body.item) {                                                   /* otherwise check for json body and parse the item out */
+    else if (req.body.item) {
+        console.log(req.body)
         var product = serialize.unserialize(req.body).item
+        console.log(product)
         try {
-            var product = product.split()
-            return res.render('index', { products: product })
-        } 
-        catch (error) {
+          var product = product.split()
+          return res.render('index', { products: product })
+        } catch (error) {
             console.log(error)
             return res.status(500)
         }
     }
     else {
-        return res.status(500)
+        return res.status(400)
     }
 })
 
-app.listen(5000)
+app.listen(5000, () => {
+    console.log('listening on port 5000...')
+})
